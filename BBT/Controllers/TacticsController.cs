8940eaa54541eb0a7ec2a8904134075ea16b95d5
amigobv <daniell.rotaru@gmail.com>
2016-09-20 22:17:44 +0200

    Project structure

---
+++ BBT/Controllers/TacticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBT.Controllers
+{
+    public class TacticsController : Controller
+    {
+        // GET: Tactics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BBT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BBT.Startup))]
+namespace BBT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
